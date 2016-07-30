509f70c416ad65437ede3d42ade19465cbc306c5
Joost Rijneveld <joost@joostrijneveld.nl>
2016-07-30 14:53:40 +0200

    Package for PyPA

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-embed-template',
+    version='0.1.0',
+    description='Override blocks in included templates',
+    long_description=open('README.md', encoding='utf-8').read(),
+    url='https://github.com/joostrijneveld/django-embed-template',
+    author='Joost Rijneveld',
+    author_email='joost@joostrijneveld.nl',
+    license='CC0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='django templates embedding',
+    packages=('django_embed_template',),
+    install_requires=['django'],  # TODO pinpoint minimal version required
+)
